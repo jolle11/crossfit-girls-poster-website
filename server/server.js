2a669b11108891d2159056998f07c9cf2dd5d55b
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-10-03 11:06:54 +0200

    Deleted commented part

--- server/server.js
+++ server/server.js
@@ -15,20 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/wods", require("./routes/wodsRoutes"));
 
-// Serve frontend
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/build")));
-	app.get("*", (req, res) => {
-		res.sendFile(
-			path.resolve(__dirname, "../", "frontend", "build", "index.html"),
-		);
-	});
-} else {
-	app.get("/", (req, res) => {
-		res.send("Please set to production");
-	});
-}
-
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
