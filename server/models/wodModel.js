f482bef0521206019b2d46731cb0b6b8cb722334
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-10-03 11:06:15 +0200

    Hidden version key for the DB

--- server/models/wodModel.js
+++ server/models/wodModel.js
@@ -1,22 +1,27 @@
 const mongoose = require("mongoose");
 
-const wodSchema = mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
+const wodSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		type: {
+			type: String,
+			required: true,
+		},
+		lines: {
+			type: Array,
+			required: true,
+		},
+		units: {
+			type: String,
+			required: true,
+		},
 	},
-	type: {
-		type: String,
-		required: true,
+	{
+		versionKey: false, // You should be aware of the outcome after set to false
 	},
-	lines: {
-		type: Array,
-		required: true,
-	},
-	units: {
-		type: String,
-		required: true,
-	},
-});
+);
 
 module.exports = mongoose.model("Wod", wodSchema);
